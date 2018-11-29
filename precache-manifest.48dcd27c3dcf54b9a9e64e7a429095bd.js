1cb2daed0b5581f282d1bd81ea70cdc1f2c6e274
David <dph0718@gmail.com>
2018-11-28 19:05:17 -0700

    Updates

---
+++ precache-manifest.48dcd27c3dcf54b9a9e64e7a429095bd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/aghast/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "04d7c7b21a11cf4d0328",
+    "url": "/aghast/static/js/runtime~main.04d7c7b2.js"
+  },
+  {
+    "revision": "13d5526720f12bae70b1",
+    "url": "/aghast/static/js/main.13d55267.chunk.js"
+  },
+  {
+    "revision": "2dd891c45704ef41f3d5",
+    "url": "/aghast/static/js/1.2dd891c4.chunk.js"
+  },
+  {
+    "revision": "13d5526720f12bae70b1",
+    "url": "/aghast/static/css/main.c3175c6d.chunk.css"
+  },
+  {
+    "revision": "8bc0fe1c3bc98035dd66b21b84eece51",
+    "url": "/aghast/index.html"
+  }
+];
